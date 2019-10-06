4dfba23cf50c0b2b4b0d92665dad3c395febab81
mtunzisteven <mtunzism@byui.edu>
2019-10-06 08:06:01 +0200

    update

---
+++ Lesson3/js/date.js
@@ -0,0 +1,9 @@
+ //Year ---------------------------------------------------------
+ var yearf = new Date(); 
+                    
+ document.getElementById("year").textContent = yearf.getFullYear();
+ //--------------------------------------------------------------
+ 
+ //Date Modified----------------------------------------------------------------------
+ document.getElementById("yp").textContent = "Last Updated: " + document.lastModified;
+ //-----------------------------------------------------------------------------------
