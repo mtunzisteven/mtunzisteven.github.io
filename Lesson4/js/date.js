5688764609eedddcaed837f65c604e2452174371
mtunzisteven <mtunzism@byui.edu>
2019-10-11 13:15:33 +0200

    preston Updates

--- Lesson4/js/date.js
+++
@@ -1,9 +0,0 @@
- //Year ---------------------------------------------------------
- var yearf = new Date(); 
-                    
- document.getElementById("year").textContent = yearf.getFullYear();
- //--------------------------------------------------------------
- 
- //Date Modified----------------------------------------------------------------------
- document.getElementById("yp").textContent = "Last Updated: " + document.lastModified;
- //-----------------------------------------------------------------------------------
