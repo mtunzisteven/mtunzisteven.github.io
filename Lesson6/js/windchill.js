f028268422afde4125adada0ca98d073edd1fe74
mtunzisteven <mtunzism@byui.edu>
2019-11-07 05:24:38 +0200

    Windchill fix

--- Lesson6/js/windchill.js
+++ Lesson6/js/windchill.js
@@ -1,9 +1,9 @@
 
-var t = parseInt(document.getElementById("high").innerHTML);
- //parseInt(document.getElementById("high").content);
+var t = parseFloat(document.getElementById("high").textContent);
+ //parseint(document.getElementById("high").content);
 //parseInt(document.getElementById("high").content);
 
-var s = parseInt(document.getElementById("speed").innerHTML);
+var s = parseFloat(document.getElementById("speed").textContent);
 //parseInt(document.getElementById("speed").content);
 //ParseInt(document.getElementById("speed").content);
 
@@ -13,7 +13,7 @@ if(s>4.8 && t>10){
     let two = (Math.pow(s,0.16));
     let three = one + 0.6215*t - 35.75*two + 0.4275*two;
 
-    document.getElementById("wind").innerHTML = Math.round(three)
+    document.getElementById("wind").textContent = Math.round(three)
 }
 else{
-    document.getElementById("wind").innerHTML = "N/A";}
+    document.getElementById("wind").textContent = "N/A";}
