bc29d8fcb1bcbab84aa2092ef980944165cb3558
mtunzisteven <mtunzism@byui.edu>
2019-11-17 06:09:17 +0200

    JavaScript

--- Lesson9/js/homepage.js
+++ Lesson9/js/homepage.js
@@ -5,7 +5,7 @@ fetch(requestURL)
       return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+      
     const towns = jsonObject['towns'];
 
     for (let i = 0; i < towns.length; i++ ) {
