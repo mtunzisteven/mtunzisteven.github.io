c99d750923d534a787fe39358312a2812b090e3d
mtunzisteven <mtunzism@byui.edu>
2019-12-21 14:36:13 +0200

    home

--- FInal Project/storm_js/rangelabels.js
+++
@@ -1,8 +0,0 @@
-
-var slider = document.getElementById("severity");
-var output = document.getElementById("rangeout");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
